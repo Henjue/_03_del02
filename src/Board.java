9c2040927395d7e45ad81ed277ed6da1c560a37f
Lucas Meldgaard <luucmeldgaard@utlook.com>
2022-10-20 02:17:30 +0200

    Opdateret Playermed mere generiske attributer og tilføjet get og setters. Omskrevet Felt til Board i stedet.

---
+++ src/Board.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Square {
+
+    Map board = new HashMap();
+
+
+    public Square(int rollTotal) {
+        moveTo(rollTotal);
+    }
+    public static void moveTo(int rollTotal) {
+
+    }
+
+}
