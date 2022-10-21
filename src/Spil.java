9efaac6c0e364e27c97cc4ce657bc67caf0590a3
Lucas Meldgaard <luucmeldgaard@utlook.com>
2022-10-22 01:41:37 +0200

    Tilføjet yderligere sprogmuligheder

--- src/Spil.java
+++ src/Spil.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Spil {
     public static int sum;
     public static void main(String[] args) {
@@ -12,8 +13,21 @@ public class Spil {
         Scanner scan = new Scanner(System.in);
         int lang = 2; //0: Danish, 1: English, 2: Swahili
 
-        String press = "'s tur, tryk Enter for at rulle med terningerne", konto = " nye kontobeholdning: ";
-        String playerString = "Spiller ", name = ", skriv dit navn.", rolled = " slog ", grats = "Tillykke ", won = " du har vundet";
+
+        String[] press_lang = new String[]         {"'s tur, tryk Enter for at rulle med terningerne", "'s turn, press Enter to roll the dice", "geuza, bonyeza Enter ili kukunja kete"};
+        String[] konto_lang = new String[]         {" nye kontobeholdning: ", "new account balance: ", "salio la akaunti: "};
+        String[] playerString_lang = new String[]  {"Spiller ", "Player ", "Wachezaji "};
+        String[] name_lang = new String[]          {", skriv dit navn.", ", write your name.", ", andika jina lako."};
+        String[] rolled_lang = new String[]        {" slog ", " rolled ", " akavingirisha "};
+        String[] grats_lang = new String[]         {"Tillykke ", "Congratulations ", "Hongera sana "};
+        String[] won_lang = new String[]           { " du har vundet", " you have won", " Umeshinda"};
+        String press = press_lang[lang];
+        String konto = konto_lang[lang];
+        String playerString = playerString_lang[lang];
+        String name = name_lang[lang];
+        String rolled = rolled_lang[lang];
+        String grats = grats_lang[lang];
+        String won = won_lang[lang];
 
         Player player1, player2;
         System.out.println(playerString + "1" + name);
