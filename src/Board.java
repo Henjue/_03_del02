d96ab78c5a2e02bc296b6cb35c589feeaf35e625
Lucas Meldgaard <luucmeldgaard@utlook.com>
2022-10-21 22:22:25 +0200

    Skift i datatype fra Hashmap til String Array i Board-klassen.

--- src/Board.java
+++ src/Board.java
@@ -1,29 +1,24 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public class Board {
 
-
-
-    Map<Integer,Integer> squares = new HashMap<Integer, Integer>()
-    {
-        {
-            put(2,  250);
-            put(3, -100);
-            put(4,  100);
-            put(5,  -20);
-            put(6,  180);
-            put(7,    0);
-            put(8,  -70);
-            put(9,   60);
-            put(10, -80);
-            put(11, -50);
-            put(12, 650);
-        }
+    String[][] felt = new String[][] { {},{},
+            {"250", "Tower", "Du er ankommet til tårnet, du finder 250 kr. i en kiste."},
+            {"-100", "Crater", "Du kører ned i et krater, det koster dig 100 kr. at komme op."},
+            {"100", "Palace gates", "Vagterne ved paladsets port betaler dig 100 kr. for at køre væk, da du blokerer for adgang!"},
+            {"-20", "Cold Desert", "Der er koldt i ørkenen, det koster dig 20 kr. at holde varmen kørende i bilen."},
+            {"180", "Walled City", "Du stjæler fra nogle turister i byen, sælger byttet og får 180 kr."},
+            {"0", "Monastery", "Du er træt og monkene i klostret giver dig et sted at sove."},
+            {"-70", "Black cave", "Du farer vildt i en mørk grotte, og er nødt til at ringe efter hjælp, hjælpen koster dig 70 kr."},
+            {"60", "Huts in the mountain", "Du finder nogle hytter i bjergene, du leder efter værdigenstande, du finder 60 kr."},
+            {"-80", "The Werewall (werewolf-wall)", "Varulvene ved varulvevæggen skræmmer livet af dig, så du kører hurtigt videre, du får en ekstra tur, men det koster dig 80 kr."},
+            {"-50", "The pit", "Din bil sidder fast i hullet, koster dig 50 kr. at få trukket den op."},
+            {"650", "Goldmine", "Du har fundet guld i guldminen og sælger det for 650 kr., du er rig!"},
     };
 
     public Board() {}
-    public int next(int rollTotal) {
-        return (int) squares.get(rollTotal);
+    public String next(int rollTotal, int info) {
+
+        //if (info == 0) {return Integer.parseInt(felt[rollTotal][0]);}
+        return felt[rollTotal][info];
     }
 }
