f4e5425eae2202a87aaf854f0231c7d124ed57b1
Anton Helsgaun <s224268@student.dtu.dk>
2022-10-28 20:32:44 +0200

    Lavet en Junit for setbalance og getbalance der tester at værdierne er ens; For alle int

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -16,10 +16,12 @@ class PlayerTest {
         }
 
      */
-    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test //Tester samtlige int-værdier
     void getBalance2() {
-        Player player = new Player("Name", 1000);
-        player.setBalance(1000);
-        assertEquals(1000, player.balance);
+        Player player = new Player("Name", 1000); //tilfældig værdi til at starte med
+        for(int i = Integer.MIN_VALUE;i<Integer.MAX_VALUE;i++) {
+            player.setBalance(i);
+            assertEquals(i, player.getBalance());
+        }
     }
 }
